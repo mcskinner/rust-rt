fc7aad95e15be6cced8cbd7746db2846cbc1a871
Michael Skinner <mcskinner@users.noreply.github.com>
2025-04-19 00:26:46 -0700

    [capstone] environment and capstone

--- src/main.rs
+++ src/main.rs
@@ -277,6 +277,41 @@ mod tests {
     }
 }
 
+#[derive(Debug, Clone, Copy)]
+struct Env {
+    gravity: Tuple,
+    wind: Tuple,
+}
+
+impl Env {
+    fn new(gravity: Tuple, wind: Tuple) -> Env {
+        Env { gravity, wind }
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+struct Projectile {
+    position: Tuple,
+    velocity: Tuple,
+}
+
+impl Projectile {
+    fn new(position: Tuple, velocity: Tuple) -> Projectile {
+        Projectile { position, velocity }
+    }
+
+    fn tick(&mut self, env: &Env) {
+        self.position = self.position + self.velocity;
+        self.velocity = self.velocity + env.gravity + env.wind;
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut p = Projectile::new(point(0.0, 1.0, 0.0), vector(1.0, 1.0, 0.0));
+    let env = Env::new(vector(0.0, -0.1, 0.0), vector(-0.01, 0.0, 0.0));
+    println!("Initial projectile position and velocity: {:?} {:?}", p.position, p.velocity);
+    while p.position.y > 0.0 {
+        p.tick(&env);
+        println!("Projectile position and velocity: {:?} {:?}", p.position, p.velocity);
+    }
 }
