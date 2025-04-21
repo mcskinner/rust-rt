142f67e19c6bc0136642ca3ce83d9319ee98042b
Michael Skinner <mcskinner@users.noreply.github.com>
2025-04-21 10:25:06 -0700

    [feature] subtract colors, approx eq

--- src/color.rs
+++ src/color.rs
@@ -1,6 +1,8 @@
-use derive_more::{Add};
+use approx::assert_abs_diff_eq;
+use approx_derive::AbsDiffEq;
+use derive_more::{Add, Sub};
 
-#[derive(Debug, Clone, Copy, PartialEq, Add)]
+#[derive(Debug, Clone, Copy, PartialEq, AbsDiffEq, Add, Sub)]
 pub struct Color {
     red: f64,
     green: f64,
@@ -28,5 +30,12 @@ mod tests {
         let c1 = color(0.9, 0.6, 0.75);
         let c2 = color(0.7, 0.1, 0.25);
         assert_eq!(c1 + c2, color(1.6, 0.7, 1.0));
+    }
+
+    #[test]
+    fn test_subtracting_colors() {
+        let c1 = color(0.9, 0.6, 0.75);
+        let c2 = color(0.7, 0.1, 0.25);
+        assert_abs_diff_eq!(c1 - c2, color(0.2, 0.5, 0.5));
     }
 }
