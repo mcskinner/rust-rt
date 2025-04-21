46eb625dc20c4087618325f021bf89c3d757859b
Michael Skinner <mcskinner@users.noreply.github.com>
2025-04-21 10:31:54 -0700

    [feature] Hadamard multiplication for colors

--- src/color.rs
+++ src/color.rs
@@ -1,6 +1,7 @@
 use approx::assert_abs_diff_eq;
 use approx_derive::AbsDiffEq;
 use derive_more::{Add, Sub, Mul};
+use std::ops::{Mul};
 
 #[derive(Debug, Clone, Copy, PartialEq, AbsDiffEq, Add, Sub, Mul)]
 pub struct Color {
@@ -11,6 +12,18 @@ pub struct Color {
 
 pub fn color(red: f64, green: f64, blue: f64) -> Color {
     Color { red, green, blue }
+}
+
+impl Mul<Color> for Color {
+    type Output = Color;
+
+    fn mul(self, other: Color) -> Color {
+        color(
+            self.red * other.red,
+            self.green * other.green,
+            self.blue * other.blue,
+        )
+    }
 }
 
 #[cfg(test)]
@@ -43,5 +56,12 @@ mod tests {
     fn test_multiplying_color_by_scalar() {
         let c = color(0.2, 0.3, 0.4);
         assert_eq!(c * 2.0, color(0.4, 0.6, 0.8));
+    }
+
+    #[test]
+    fn test_multiplying_colors() {
+        let c1 = color(1.0, 0.2, 0.4);
+        let c2 = color(0.9, 1.0, 0.1);
+        assert_abs_diff_eq!(c1 * c2, color(0.9, 0.2, 0.04));
     }
 }
