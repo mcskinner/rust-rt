267a7ef1bb98f02a70654acb5ddf6745698598cf
Michael Skinner <mcskinner@users.noreply.github.com>
2025-04-25 23:12:57 -0700

    [feature] extract hit from intersection list

--- src/intersection.rs
+++ src/intersection.rs
@@ -1,6 +1,6 @@
 use crate::sphere::Sphere;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub struct Intersection<'a> {
     pub t: f64,
     pub object: &'a Sphere,
@@ -10,6 +10,20 @@ impl Intersection<'_> {
     pub fn new(t: f64, object: &Sphere) -> Intersection {
         Intersection { t, object }
     }
+}
+
+pub fn hit<'a>(xs: &'a [Intersection<'a>]) -> Option<&'a Intersection<'a>> {
+    let mut hit: Option<&Intersection> = None;
+
+    for i in xs {
+        if i.t > 0.0 {
+            if hit.is_none() || i.t < hit.unwrap().t {
+                hit = Some(i);
+            }
+        }
+    }
+
+    hit
 }
 
 #[cfg(test)]
@@ -35,5 +49,43 @@ mod tests {
         assert_eq!(xs.len(), 2);
         assert_eq!(xs[0].t, 1.0);
         assert_eq!(xs[1].t, 2.0);
+    }
+
+    #[test]
+    fn test_hit() {
+        let s = Sphere::new();
+        let i1 = Intersection::new(1.0, &s);
+        let i2 = Intersection::new(-1.0, &s);
+        let xs = vec![i2, i1];
+        assert_eq!(hit(&xs).unwrap().t, 1.0);
+    }
+
+    #[test]
+    fn test_hit_with_some_intersections_negative() {
+        let s = Sphere::new();
+        let i1 = Intersection::new(-1.0, &s);
+        let i2 = Intersection::new(1.0, &s);
+        let xs = vec![i1, i2];
+        assert_eq!(hit(&xs).unwrap().t, 1.0);
+    }
+
+    #[test]
+    fn test_hit_with_all_negative_intersections() {
+        let s = Sphere::new();
+        let i1 = Intersection::new(-2.0, &s);
+        let i2 = Intersection::new(-1.0, &s);
+        let xs = vec![i2, i1];
+        assert_eq!(hit(&xs), None);
+    }
+
+    #[test]
+    fn test_hit_is_always_the_lowest_positive_intersection() {
+        let s = Sphere::new();
+        let i1 = Intersection::new(5.0, &s);
+        let i2 = Intersection::new(7.0, &s);
+        let i3 = Intersection::new(-3.0, &s);
+        let i4 = Intersection::new(2.0, &s);
+        let xs = vec![i1, i2, i3, i4];
+        assert_eq!(hit(&xs).unwrap(), &xs[3]);
     }
 }
