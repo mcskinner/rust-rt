c80f479fb393726f490ea90e94fa7b9e6cb45f25
Michael Skinner <mcskinner@users.noreply.github.com>
2025-04-25 17:56:18 -0700

    [feature] point along a ray

--- src/ray.rs
+++ src/ray.rs
@@ -9,6 +9,10 @@ impl Ray {
     fn new(origin: Tuple, direction: Tuple) -> Ray {
         Ray { origin, direction }
     }
+
+    fn position(&self, t: f64) -> Tuple {
+        self.origin + self.direction * t
+    }
 }
 
 #[cfg(test)]
@@ -22,5 +26,14 @@ mod tests {
         let r = Ray::new(origin, direction);
         assert_eq!(r.origin, point(1.0, 2.0, 3.0));
         assert_eq!(r.direction, vector(4.0, 5.0, 6.0));
+    }
+
+    #[test]
+    fn test_computing_a_point_from_a_distance() {
+        let r = Ray::new(point(2.0, 3.0, 4.0), vector(1.0, 0.0, 0.0));
+        assert_eq!(r.position(0.0), point(2.0, 3.0, 4.0));
+        assert_eq!(r.position(1.0), point(3.0, 3.0, 4.0));
+        assert_eq!(r.position(-1.0), point(1.0, 3.0, 4.0));
+        assert_eq!(r.position(2.5), point(4.5, 3.0, 4.0));
     }
 }
